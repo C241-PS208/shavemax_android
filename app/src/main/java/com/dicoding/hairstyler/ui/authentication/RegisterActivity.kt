4cc9f7256ddb5f74f54a664dade417009b7b9dcd
diroaryo <iamaryodiro@gmail.com>
2024-06-03 23:38:43 +0700

    ui: spinner for selecting gender
    - gender spinner
    - couple adjustment for dark theme

--- app/src/main/java/com/dicoding/hairstyler/ui/authentication/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/hairstyler/ui/authentication/RegisterActivity.kt
@@ -2,12 +2,20 @@ package com.dicoding.hairstyler.ui.authentication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.hairstyler.R
 import com.dicoding.hairstyler.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityRegisterBinding
+    private var selectedGender: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -26,5 +34,33 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+
+        setupSpinner()
+    }
+
+    private fun setupSpinner() {
+        val genderOptions = arrayOf("Male", "Female")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerGender.adapter = adapter
+
+        binding.spinnerGender.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                selectedGender = genderOptions[position]
+                Log.d(TAG, "selectedGender: $selectedGender")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "RegisterActivity"
     }
 }
