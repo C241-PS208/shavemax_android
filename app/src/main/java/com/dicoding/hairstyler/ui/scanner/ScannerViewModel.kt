9fb1d507acff9f642cd3f4908cba1fff2245cdb9
diroaryo <iamaryodiro@gmail.com>
2024-06-07 13:43:10 +0700

    refactor: folder/class name, icon, and navigations

--- app/src/main/java/com/dicoding/hairstyler/ui/scanner/ScannerViewModel.kt
+++ app/src/main/java/com/dicoding/hairstyler/ui/scanner/ScannerViewModel.kt
@@ -1,13 +1,13 @@
-package com.dicoding.hairstyler.ui.dashboard
+package com.dicoding.hairstyler.ui.scanner
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ScannerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is scanner Fragment"
     }
     val text: LiveData<String> = _text
 }
