09d91eb3477d2be5bb1647f02b7b55e957d2f0a9
diroaryo <iamaryodiro@gmail.com>
2024-06-07 16:34:38 +0700

    ui: scanner page

--- app/src/main/java/com/dicoding/hairstyler/ui/scanner/ScannerFragment.kt
+++ app/src/main/java/com/dicoding/hairstyler/ui/scanner/ScannerFragment.kt
@@ -28,10 +28,6 @@ class ScannerFragment : Fragment() {
         _binding = FragmentScannerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        scannerViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
